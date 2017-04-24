10d3d2af5e04d28971b9c838d004ca93c2cd157f
Monster-Girl <1104306242@qq.com>
2017-04-24 19:58:36 +0800

    哈希表开放定址法插入的实现

--- Hash.h
+++ Hash.h
@@ -1,7 +1,9 @@
 #include<iostream>
-
 using namespace std;
-enum  state
+#include<vector>
+#include<string>
+
+enum state
 {
 	EMPTY,
 	EXIST,
@@ -15,17 +17,27 @@ struct HashNode
 	state _s;
 };
 
-struct _HashFunc 
+template<class K>
+struct _HashFunc
 {
-	static size_t BKDRHash(const char*str) 
+	size_t operator()(const K& key)
+	{
+		return key;
+	}
+};
+
+template<>
+struct _HashFunc<string>
+{
+	static size_t BKDRHash(const char* str)
 	{
 		unsigned int seed = 131;// 31 131 1313 13131 131313   
-		unsigned int hash= 0;   
-		while(*str)    
-		{        
-			hash = hash * seed+ (*str++);   
-		}    
-		return(hash & 0x7FFFFFFF); 
+		unsigned int hash = 0;
+		while (*str)
+		{
+			hash = hash * seed + (*str++);
+		}
+		return(hash & 0x7FFFFFFF);
 	}
 
 	size_t operator()(const string& key)
@@ -34,7 +46,7 @@ struct _HashFunc
 	}
 };
 
-template<class T, class V>
+template<class K, class V,class HashFunc=_HashFunc<K>>
 class HashTable
 {
 	typedef HashNode<K, V> Node;
@@ -49,38 +61,79 @@ public:
 		_tables.resize(n);
 	}
 
-	pair<Node*,bool> Insert(const pair<K, V>& kv)
+	pair<Node*, bool> Insert(const pair<K, V>& kv)
 	{
 		_check();
-		size_t index = HashFunc(kv.first);
+		size_t index = _HashFunc(kv.first);
 		while (_tables[index]._s == EXIST)
 		{
-			if (_table[index]._kv.first == kv)
-				return make_pair(NULL, false);
+			if (_tables[index]._kv.first == kv.first)
+				return make_pair(&_tables[index], false);
 
 			index += 1;
 			if (index == _tables.size())
 				index = 0;
 		}
 
-		_tables[index].kv = kv;
+		_tables[index]._kv = kv;
 		_tables[index]._s = EXIST;
 		++_size;
-		returb make_pair(_tables[index]._s, true);
+		return make_pair(&_tables[index], true);
 	}
-	
+
+	Node* Find(const K& key)
+	{
+		size_t index = _HashFunc(key);
+		while (_tables[index]._s != EMPTY)
+		{
+			if (_tables[index]._kv.first == key && _tables[index]._s!=DELETE)
+				return &_tables[index];
+			index += 1;
+			if (index == _tables.size())
+				index = 0;
+			return NULL;
+		}
+	}
+
+	bool Remove(const K& key)
+	{
+		Node* cur = Find(key);
+		if (cur)
+		{
+			cur->_s = DELETE;
+			--_size;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	V& operator[](const K& key)
+	{
+		pair<Node*, bool> node = Insert(make_pair(key, V()));
+		return (node.first->_kv).second;
+	}
+
+
 protected:
+
+	size_t _HashFunc(const K& key)
+	{
+		HashFunc hf;
+		return hf(key) % _tables.size();
+	}
+
 	void _check()
 	{
 		if (_tables.size() == 0)
 		{
-			_tables.GetNextPrime(0);
+			_tables.resize(GetNextPrime(0));
 		}
 
-		else if (_size * 10 / _tables.size()==7)    //负载因子大于7就需要考虑增容
+		else if (_size * 10 / _tables.size() == 7)    //负载因子大于7就需要考虑增容
 		{
 			size_t newSize = GetNextPrime(_tables.size());
-			HashTable<K, V> newHT(newSize);
+			HashTable<K, V,HashFunc> newHT(newSize);
 			for (size_t i = 0; i < _tables.size(); i++)
 			{
 				if (_tables[i]._s == EXIST)
@@ -90,7 +143,7 @@ protected:
 		}
 	}
 
-	void Swap(HashTable<K, V>& ht)
+	void Swap(HashTable<K, V,HashFunc>& ht)
 	{
 		swap(_size, ht._size);
 		_tables.swap(ht._tables);
@@ -99,14 +152,14 @@ protected:
 
 	size_t GetNextPrime(size_t num)
 	{
-		const int _PrimeSize = 28;    
-		static const unsigned long _PrimeList[_PrimeSize] = 
-		{ 53ul, 97ul, 193ul, 389ul, 769ul, 
-		  1543ul, 3079ul, 6151ul, 12289ul, 24593ul, 
-		  49157ul, 98317ul, 196613ul, 393241ul, 786433ul, 
-		  1572869ul, 3145739ul, 6291469ul, 12582917ul, 25165843ul, 
-		  50331653ul,  100663319ul, 201326611ul, 402653189ul, 805306457ul, 
-		  1610612741ul, 3221225473ul, 4294967291ul 
+		const int _PrimeSize = 28;
+		static const unsigned long _PrimeList[_PrimeSize] =
+		{ 53ul, 97ul, 193ul, 389ul, 769ul,
+		1543ul, 3079ul, 6151ul, 12289ul, 24593ul,
+		49157ul, 98317ul, 196613ul, 393241ul, 786433ul,
+		1572869ul, 3145739ul, 6291469ul, 12582917ul, 25165843ul,
+		50331653ul, 100663319ul, 201326611ul, 402653189ul, 805306457ul,
+		1610612741ul, 3221225473ul, 4294967291ul
 		};
 
 		for (size_t i = 0; i < _PrimeSize; i++)
@@ -125,5 +178,29 @@ protected:
 
 void TestHashTables()
 {
-	HashTable<int, int>ht;
+	HashTable<int,int> ht;
+	int a[] = { 89, 18, 49, 58, 9 };
+
+	for(size_t i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
+	{
+		 ht.Insert(make_pair(a[i], i));
+	}
+
+	ht.Insert(make_pair(3, 1));
+	ht.Insert(make_pair(43, 1));
+	ht.Insert(make_pair(24, 1));
+	ht.Insert(make_pair(109, 1));
+	ht.Insert(make_pair(26, 1));
+	ht.Insert(make_pair(8, 1));
+
+	cout << ht.Find(58) << endl;
+	cout << ht.Find(1) << endl;
+
+	cout << ht.Remove(58) << endl;
+	cout << ht.Find(58) << endl;
+	cout << ht.Remove(58) << endl;
+
+	HashTable<string, string> dict;
+	dict["left"] = "左边";
+	dict["left"] = "剩余";
 }
