8b897e722053e079cd80f2475c91633a3497e01f
Monster-Girl <1104306242@qq.com>
2017-05-14 23:34:04 +0800

    堆排序算法的实现

--- Heap.h
+++ Heap.h
@@ -1,185 +1,54 @@
 #include<iostream>
-#include<vector>
-#include<assert.h>
 using namespace std;
+#include<assert.h>
 
-
-//大堆
-//template<class T>
-//class Heap
-//{
-//public:
-//	Heap()
-//	{}
-//
-//	Heap(T* a, size_t n)
-//	{
-//		_a.reserve(n); 
-//		for (size_t i = 0; i < n; i++)
-//		{
-//			_a.push_back(a[i]);
-//		}
-//
-//		for (int i = _a.size() - 2 / 2; i >= 0; --i)
-//			_AdjustDown(i);
-//	}
-//
-//	void Push(const T& x)
-//	{
-//		_a.push_back(x);
-//		_AdjustUp(_a.size() - 1);   //调整的是插入的最后一个元素，输入最后一个坐标
-//	}
-//
-//	void Pop()
-//	{
-//		assert(!_a.empty());
-//		swap(_a[0], _a[_a.size() - 1]);
-//		_a.pop_back();
-//		_AdjustDown(0);
-//	}
-//
-//protected:
-//	void _AdjustDown(int root)
-//	{
-//		int parent = root;
-//		int child = root * 2 + 1;
-//		while (child < _a.size())
-//		{
-//			if (child + 1 < _a.size() && _a[child] < _a[child + 1])
-//				++child;
-//			if (_a[child]>_a[parent])
-//			{
-//				swap(_a[child], _a[parent]);
-//				parent = child;
-//				child = parent * 2 + 1;
-//			}
-//			else
-//				break;
-//		}
-//	}
-//
-//	void _AdjustUp(int child)
-//	{
-//		int parent = (child - 1) >> 1;
-//		while (child > 0)
-//		{
-//			if (_a[child] > _a[parent])
-//			{
-//				swap(_a[child], _a[parent]);
-//				child = parent;
-//				parent = (child - 1) >> 1;
-//			}
-//			else
-//				break;
-//		}
-//	}
-//
-//protected:
-//	vector<T> _a;
-//};
-
-
-//任意堆，用仿函数进行实现
-template<class T>
-struct Less
+void PrintSort(int* a, size_t n)
 {
-	bool operator()(const T& l, const T& r) const
+	for (size_t i = 0; i < n; i++)
 	{
-		return l < r;
+		cout << a[i] << " ";
 	}
-};
+	cout << endl;
+}
 
-template <class T>
-struct Great
+void Adjust(int* a, size_t n, int root)
 {
-	bool operator()(const T& l, const T& r) const
+	int parent = root;
+	int child = parent * 2 + 1;
+	while (child < n)
 	{
-		return l > r;
-	}
-};
-
-template<class T,class Compare>
-class Heap
-{
-public:
-	Heap()
-	{}
-
-	Heap(T* a, size_t n)
-	{
-		_a.reserve(n); 
-		for (size_t i = 0; i < n; i++)
+		if (child + 1 < n && a[child] < a[child + 1])
+			++child;
+		if (a[child]>a[parent])
 		{
-			_a.push_back(a[i]);
-		}
-
-		for (int i = _a.size() - 2 / 2; i >= 0; --i)
-			_AdjustDown(i);
+			swap(a[child], a[parent]);
+			parent = child;
+			child = parent * 2 + 1;
+		} 
+		else
+			break;
 	}
+}
 
-	void Push(const T& x)
-	{
-		_a.push_back(x);
-		_AdjustUp(_a.size() - 1);   //调整的是插入的最后一个元素，输入最后一个坐标
-	}
-
-	void Pop()
-	{
-		assert(!_a.empty());
-		swap(_a[0], _a[_a.size() - 1]);
-		_a.pop_back();
-		_AdjustDown(0);
-	}
-
-protected:
-	void _AdjustDown(int root)
-	{
-		Compare comFunc;
-		int parent = root;
-		int child = root * 2 + 1;
-		while (child < _a.size())
-		{
-			if (child + 1 < _a.size() && _a[child] < _a[child + 1])
-				++child;
-			if (comFunc( _a[child],_a[parent]))
-			{
-				swap(_a[child], _a[parent]);
-				parent = child;
-				child = parent * 2 + 1;
-			}
-			else
-				break;
-		}
-	}
-
-	void _AdjustUp(int child)
-	{
-		Compare comFunc;
-		int parent = (child - 1) >> 1;
-		while (child > 0)
-		{
-			if (comFunc(_a[child], _a[parent]))
-			{
-				swap(_a[child], _a[parent]);
-				child = parent;
-				parent = (child - 1) >> 1;
-			}
-			else
-				break;
-		}
-	}
-
-protected:
-	vector<T> _a;
-};
-
-
-
-
-void TestHeap()
+void HeapSort(int* a, size_t n)
 {
-	int a[] = { 10, 11, 13, 12, 16, 18, 15, 17, 14, 19 };
-	Heap<int,Less<int>> h(a, 10);
-	h.Push(30);
-	h.Pop();
+	assert(a);
+	for (int i = (n - 2) / 2; i >= 0; --i)
+		Adjust(a, n, i);
+	size_t end = n - 1;
+	while (end > 0)
+	{
+		
+		swap(a[0] , a[end]);
+		Adjust(a, end, 0);
+		--end;
+		PrintSort(a, 10);
+	}
+}
+
+void TestHeapSort()
+{
+	int a[] = { 2, 5, 4, 9, 3, 6, 8, 7, 1, 0 };
+	HeapSort(a, 10);
+
 }
